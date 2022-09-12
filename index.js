a7f6ee1b7d04d369051edf7e1e633d867ce1a025
unknown <mdatislam>
2022-09-12 23:34:15 +0600

    approve & rejet feature add

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -22,9 +22,9 @@ async function run() {
   try {
     await client.connect();
     const userCollection = client.db("BL-Operation").collection("user");
-    const pgRunDataCollection = client.db("BL-Operation").collection("pgRunData");
-
-    
+    const pgRunDataCollection = client
+      .db("BL-Operation")
+      .collection("pgRunData");
 
     // get user info API & token issue API
     app.put("/user/:email", async (req, res) => {
@@ -47,40 +47,56 @@ async function run() {
     });
 
     // pgRunData update into data base API
-    app.post('/pgRunData', async (req, res) => {
-      const pgData = req.body
+    app.post("/pgRunData", async (req, res) => {
+      const pgData = req.body;
       //console.log(pgData)
-      const result = await pgRunDataCollection.insertOne(pgData)
-      res.send(result)
-    })
+      const result = await pgRunDataCollection.insertOne(pgData);
+      res.send(result);
+    });
 
-    app.get('/pgRunList',async(req,res)=>{
-      const email = req.query.email
+    app.get("/pgRunAllList",async (req,res) => {
+      const email = req.query.email;;
       //console.log(email)
-      const filter= {email:email}
-      const result = await pgRunDataCollection.find(filter).toArray()
-      res.send(result)
-    })
+      const filter = { pgRunnerEmail: email };
+      const result = await pgRunDataCollection.find(filter).toArray();
+      res.send(result);
+    });
 
-    app.get('/ApprovalList',async(req,res)=>{
+    app.put("/pgRunList/:id", async (req, res) => {
+      const id = req.params.id;
+      const approvalInfo = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: approvalInfo,
+      };
+      const result = await pgRunDataCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    app.get("/ApprovalList", async (req, res) => {
       const email = req.query.email;
-      const filter = { onCallEmail: email };
+      const filter = {
+        onCallEmail: email,
+        status: "Pending",
+      };
       //console.log(filter)
       const result = await pgRunDataCollection.find(filter).toArray();
       res.send(result);
-    })
+    });
 
-    app.get('/userList', async (req, res) => {
-      const result = await userCollection.find().toArray()
-      res.send(result)
-    })
-
+    app.get("/userList", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
   } finally {
   }
 }
 run().catch(console.dir);
-
-
 
 app.get("/", (req, res) => {
   res.send("we are tiger from Rangpur");
