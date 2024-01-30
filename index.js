b940feb9254f28742f3004fedc2694d08bf948e0
mdatislam <atiq05099@gmail.com>
2024-01-30 20:49:57 +0600

    more'

--- index.js
+++ index.js
@@ -9,11 +9,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
- const corsOptions = {
-  origin: 'https://bl-operation.web.app/',
+const corsOptions = {
+  origin: 'https://bl-operation.web.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
 }; 
+
+app.post('/jwt', async (req, res) => {
+     // console.log('jwt hit korese')
+      const userEmail = await req.body
+      //console.log(userInfo)
+      const token = jwt.sign({
+        email: userEmail,
+      }, process.env.ACCESS_TOKEN, { expiresIn: "1hr" })
+      res.send({ token: token })
+    }) 
 
 //https://bl-operation-server-8udwslvjt-mdatislam.vercel.app
 //https://backend.bloperation.com
